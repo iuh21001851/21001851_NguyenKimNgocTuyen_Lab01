61b99620049acd8a77bad35bbd0eadc568f9e3c6
iuh21001851 <tuyennguyen224122@gmail.com>
2024-08-19 13:53:29 +0700

    cau3

--- bai3/index.js
+++ bai3/index.js
@@ -1,18 +1,3 @@
-//Bai 1: Print numbers from 1 to 10 
-var htmlBai1 = document.querySelector('#bai1');
-var bai1='';
-for(var i=1;i<=10; i++){
-    bai1+=`<span>${i} </span>`
-}
-htmlBai1.innerHTML+=bai1;
-//Bai 2: Print the odd numbers less than 100
-var htmlBai2 = document.querySelector('#bai2');
-var bai2='';
-for(var i=1;i<=100; i+=2) {
-    bai2+=`<span>${i} </span>`;
-}
-htmlBai2.innerHTML+=bai2;
-
 //Bai 3: Print the multiplication table with 7
 var htmlBai3 = document.querySelector('#bai3');
 var bai3='';
