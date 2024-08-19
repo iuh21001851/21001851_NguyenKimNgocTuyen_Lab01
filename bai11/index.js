1cb3dca295fbeb0beafb40576ad6fa8a41a8ea86
iuh21001851 <tuyennguyen224122@gmail.com>
2024-08-19 23:56:25 +0700

    bai11

---
+++ bai11/index.js
@@ -0,0 +1,14 @@
+//Bai 9: Calculate the sum of numbers in an array of numbers
+var htmlBai = document.querySelector('#bai');
+var btn = document.querySelector('#tinh');
+btn.onclick=function(){
+    var htmlInput = document.querySelector('input[name="mang"]').value.split(';').map(Number)
+    var tong=0;
+    var lenght =htmlInput.length;
+    htmlInput.forEach(element => {
+        tong+=element;
+    });
+    document.querySelector('input[name="kq"]').value=tong/lenght;
+}
+
+
