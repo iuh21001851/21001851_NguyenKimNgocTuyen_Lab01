bb09461b3bf4463501dfd173be41a41794a79bf9
iuh21001851 <tuyennguyen224122@gmail.com>
2024-08-19 13:45:27 +0700

    cau2

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 //Bai 1: Print numbers from 1 to 10 
+var htmlBai1 = document.querySelector('#bai1');
+var bai1='';
 for(var i=1;i<=10; i++){
-    console.log(i);
+    bai1+=`<span>${i} </span>`
 }
+htmlBai1.innerHTML+=bai1;
+//Bai 2: Print the odd numbers less than 100
+var htmlBai2 = document.querySelector('#bai2');
+var bai2='';
+for(var i=1;i<=100; i+=2) {
+    bai2+=`<span>${i} </span>`;
+}
+htmlBai2.innerHTML+=bai2;
