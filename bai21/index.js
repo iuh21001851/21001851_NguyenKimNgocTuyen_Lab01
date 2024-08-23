b68a3da2f891170817fcaea2b772453117d1e9cb
iuh21001851 <tuyennguyen224122@gmail.com>
2024-08-24 02:15:04 +0700

    delete bai22

--- bai21/index.js
+++ bai21/index.js
@@ -1,18 +1,22 @@
-//Bai 21: Rotate an array to the right 1 position 
+//Bai 22: Reverse an array
 var htmlBai = document.querySelector('#bai');
 var btn = document.querySelector('#ketqua');
-function rotateRight(arr){
+function reverse(arr){
     if(arr.length===0) return arr;
-    var s= arr[arr.length-1];
-    for(i=arr.length-1;i>0; i-=1) arr[i]=arr[i-1];
-    arr[0]=s;
+    var left=0, right=arr.length-1;
+    while(left<=right){
+        var tmp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=tmp;
+        left++; right--;
+    }
     return arr;
 }
 btn.onclick=function(){
     var htmlInput = document.querySelector('input[name="mang"]').value.split(';').map(Number)
     
     
-   document.querySelector('input[name="kq"]').value=rotateRight(htmlInput).join(';');
+   document.querySelector('input[name="kq"]').value=reverse(htmlInput).join(';');
 }
 
 
